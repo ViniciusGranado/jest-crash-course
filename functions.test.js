0efdf809644715626b5753b6feaa68a872bc3ee9
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-06-03 17:34:16 -0300

    Added NULL test

--- functions.test.js
+++ functions.test.js
@@ -1,9 +1,16 @@
 const functions = require("./functions");
 
+// Equal to
 test("Adds 2 + 2 to equal 4", () => {
   expect(functions.add(2, 2)).toBe(4);
 });
+
+//Not equal to
 test("Adds 2 + 2 to NOT equal 5", () => {
   expect(functions.add(2, 2)).not.toBe(5);
 });
 
+//Null
+test('Should be null', () => {
+  expect(functions.isNull()).toBeNull();
+});
