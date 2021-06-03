640756b9a5094d2694de0b3e5b82d13661f172bb
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-06-03 17:37:36 -0300

    Added FALSY test

--- functions.test.js
+++ functions.test.js
@@ -5,12 +5,17 @@ test("Adds 2 + 2 to equal 4", () => {
   expect(functions.add(2, 2)).toBe(4);
 });
 
-//Not equal to
+// Not equal to
 test("Adds 2 + 2 to NOT equal 5", () => {
   expect(functions.add(2, 2)).not.toBe(5);
 });
 
-//Null
+// Null
 test('Should be null', () => {
   expect(functions.isNull()).toBeNull();
 });
+
+// Falsy
+test('Should be falsy', () => {
+  expect(functions.checkValue(null)).toBeFalsy();
+})
