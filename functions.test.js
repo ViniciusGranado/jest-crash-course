8a970880ec6da0109c03009232bbfe5b9ffc11b9
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-06-04 12:36:24 -0300

    Added Async data with promisses test

--- functions.test.js
+++ functions.test.js
@@ -11,40 +11,49 @@ test("Adds 2 + 2 to NOT equal 5", () => {
 });
 
 // Null
-test('Should be null', () => {
+test("Should be null", () => {
   expect(functions.isNull()).toBeNull();
 });
 
 // Falsy
-test('Should be falsy', () => {
+test("Should be falsy", () => {
   expect(functions.checkValue(null)).toBeFalsy();
-})
+});
 
 // Equal to object
-test('Should be Vinicius Granado object', () => {
+test("Should be Vinicius Granado object", () => {
   expect(functions.createUser()).toEqual({
-    firstName: 'Vinicius',
-    lastName: 'Granado',
+    firstName: "Vinicius",
+    lastName: "Granado",
     test: undefined,
   });
 });
 
 // Less than and greater than
-test('Should be under 1600', () => {
+test("Should be under 1600", () => {
   const load1 = 800;
   const load2 = 700;
 
   expect(load1 + load2).toBeLessThanOrEqual(1600);
-})
+});
 
 // Regex
-test('There is no I in team', () => {
-  expect('team').not.toMatch(/I/);
-})
+test("There is no I in team", () => {
+  expect("team").not.toMatch(/I/);
+});
 
 // Arrays
-test('Admin should be in usernames', () => {
-  const usernames = ['john', 'karen', 'admin'];
+test("Admin should be in usernames", () => {
+  const usernames = ["john", "karen", "admin"];
 
-  expect(usernames).toContain('admin');
-})
+  expect(usernames).toContain("admin");
+});
+
+// Async data
+//Promisse
+test("User fetched name should be Leanne Graham", () => {
+  expect.assertions(1);
+  return functions.fetchUser().then((data) => {
+    expect(data.name).toEqual("Leanne Graham");
+  });
+});
