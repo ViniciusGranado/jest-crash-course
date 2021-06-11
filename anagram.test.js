a64f2869696448e2ed31438ef108ff2a239d9c93
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-06-11 17:40:07 -0300

    Added anagram test

--- anagram.test.js
+++ anagram.test.js
@@ -3,3 +3,7 @@ const isAnagram = require('./anagram');
 test('isAnagram function exists', () => {
   expect(typeof isAnagram).toEqual('function');
 })
+
+test('cinema is an anagram of iceman', () => {
+  expect(isAnagram('cinema', 'iceman')).toBeTruthy();
+})
