91c1face92b8839b69e6551dc2d2174622503087
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-06-12 21:34:06 -0300

    Finished anagram tests

--- anagram.test.js
+++ anagram.test.js
@@ -7,3 +7,11 @@ test('isAnagram function exists', () => {
 test('cinema is an anagram of iceman', () => {
   expect(isAnagram('cinema', 'iceman')).toBeTruthy();
 })
+
+test('Dormitory is an anagram of dirty room##', () => {
+  expect(isAnagram('Dormitory', 'dirty room##')).toBeTruthy();
+})
+
+test('Hello is NOT an anagram of Aloha', () => {
+  expect(isAnagram('Hello', 'Aloha')).toBeFalsy();
+})
